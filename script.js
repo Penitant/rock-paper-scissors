ebd4b2f60e75172bf872cb09b68f9f58c158c0b8
Penitant <162775288+Penitant@users.noreply.github.com>
2024-11-12 16:08:39 +0530

    Compare and Give Winner, Score yet to be fixed.

--- script.js
+++ script.js
@@ -1,15 +1,40 @@
-// Initialising the variables
+ // Initialising the variables
 let computerGeneratedInput, userInput, computernNormalisation, userNormalisation  = 0;
-
-
+let playerScore, computerScore, counter = 0;
 
 // Round - 1
 // Generating a random between 1 and 3;
-function randomIntegerGenerator(max) {
-    return Math.floor(Math.random() * max);
+function choiceMaker(max) {
+    let x = Math.floor(Math.random() * max);
+    if (x == 0) {
+        return "Rock";
+    }
+    else if (x == 1) {
+        return "Paper";
+    }
+    else {
+        return "Scissors";
+    }
 }
 
+computerGeneratedInput = choiceMaker(3);
+let computerInputLength = computerGeneratedInput.length;
+
 // Input the User's choice
-// Setting Nomralised integer for the user's input
+userInput = prompt("Enter your choice Rock, Paper, Scissors");
+let userInputLength = userInput.length;
+
 // Comparing the User's choice vs The computer's choice 
-// Score / Winner Declaration.
+if(computerInputLength == userInputLength) {
+    console.log("Tie");
+} 
+else if(computerInputLength == 4 && userInputLength == 5 || computerInputLength == 5 && userInputLength == 8 || computerInputLength == 8 && userInputLength == 4) {
+    playerScore += 1;
+    console.log(`Player Wins, Score = ${playerScore}`);
+    
+}
+else {
+    computerScore += 1;
+    console.log(`Computer Wins, Score = ${computerScore}`);
+    
+}
