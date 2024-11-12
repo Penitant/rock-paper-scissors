803f9fc937153567edf4277d2d3708801c2639b2
Penitant <162775288+Penitant@users.noreply.github.com>
2024-11-12 14:44:05 +0530

    Generate a random integer between 0 and the max number provided

---
+++ script.js
@@ -0,0 +1,15 @@
+// Initialising the variables
+let computerGeneratedInput, userInput, computernNormalisation, userNormalisation  = 0;
+
+
+
+// Round - 1
+// Generating a random between 1 and 3;
+function randomIntegerGenerator(max) {
+    return Math.floor(Math.random() * max);
+}
+
+// Input the User's choice
+// Setting Nomralised integer for the user's input
+// Comparing the User's choice vs The computer's choice 
+// Score / Winner Declaration.
