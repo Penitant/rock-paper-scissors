8a9b2ffb3884ace2dc1340a2c938fb5116a2d715
Penitant <162775288+Penitant@users.noreply.github.com>
2024-11-13 10:59:39 +0530

    Completes the project.

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
  // Initialising the variables
-let computerGeneratedInput, userInput, computernNormalisation, userNormalisation  = 0;
-let counter = 0;
-let playerScore = 0;
+let getComputerChoice, getHumanChoice = 0;
+let humanScore = 0;
 let computerScore = 0;
 
-// Round - 1
+// User input for how many rounds to play
+let roundNumber = parseInt(prompt("Enter the number of rounds you want to play."));
+
 // Generating a random between 1 and 3;
 function choiceMaker(max) {
     let x = Math.floor(Math.random() * max);
@@ -19,24 +20,39 @@ function choiceMaker(max) {
     }
 }
 
-computerGeneratedInput = choiceMaker(3);
-let computerInputLength = computerGeneratedInput.length;
+while(roundNumber > 0) {
 
-// Input the User's choice
-userInput = prompt("Enter your choice Rock, Paper, Scissors");
-let userInputLength = userInput.length;
+    getComputerChoice = choiceMaker(3);
+    let computerInputLength = getComputerChoice.length;
 
-// Comparing the User's choice vs The computer's choice 
-if(computerInputLength == userInputLength) {
-    console.log("Tie");
-} 
-else if(computerInputLength == 4 && userInputLength == 5 || computerInputLength == 5 && userInputLength == 8 || computerInputLength == 8 && userInputLength == 4) {
-    playerScore = playerScore + 1;
-    console.log(`Player Wins, Score = ${playerScore}`);
-    
+    // Input the User's choice
+    getHumanChoice = prompt("Enter your choice Rock, Paper, Scissors");
+    let getHumanChoiceLength = getHumanChoice.length;
+
+    // Comparing the User's choice vs The computer's choice and printing Output
+    console.log(roundNumber);
+    if(computerInputLength == getHumanChoiceLength) {
+        console.log("Tie");
+    } 
+    else if(computerInputLength == 4 && getHumanChoiceLength == 5 || computerInputLength == 5 && getHumanChoiceLength == 8 || computerInputLength == 8 && getHumanChoiceLength == 4) {
+        humanScore = humanScore + 1;
+        console.log(`Player Wins, Score = ${humanScore}`);
+
+    }
+    else {
+        computerScore = computerScore + 1;
+        console.log(`Computer Wins, Score = ${computerScore}`);
+
+    }
+    roundNumber -= 1;
+}
+
+if(humanScore == computerScore) {
+    console.log("Result of Match : TIE!!!");   
+}
+else if(humanScore > computerScore) {
+    console.log("Result of Match : PLAYER WINS!!!");
 }
 else {
-    computerScore = computerScore + 1;
-    console.log(`Computer Wins, Score = ${computerScore}`);
-    
+    console.log("Result of Match : COMPUTER WINS!!!");
 }
